fca9faf02a95f43541007a22c94ae880083476f8
Lucy Wall <lhwall@gmail.com>
2019-01-25 21:14:57 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -5,3 +5,14 @@ function getRepositories(){
   req.open("GET", "http://api.github.com/users/" + username + "/repos")
   req.send()
 }
+
+function showRepositories(){
+  let repos = JSON.parse(this.responseText)
+  let repoList = 'ul> ${repos
+    .map{
+    r=>
+    '<li>' + r.name + ' - <a href = "#" data-repo="' + r.name + '" onclick="getCommits(this)"Get Commits</a></li>'
+  ).join('')}<ul>'
+
+  document.getElementById("repositories").innerHTML=repoList;
+  }
