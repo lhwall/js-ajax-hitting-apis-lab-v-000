ca9158ee0847b32c70963d8da31dc521bdef03a5
Lucy Wall <lhwall@gmail.com>
2019-01-25 20:13:06 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // your code here
+function getRepositories(){
+  const req = new XMLHttpRequest()
+  username = document.getElementById("username")
+  req.open("GET", "http://api.github.com/users/" + username + "/repos")
+  req.send()
+}
